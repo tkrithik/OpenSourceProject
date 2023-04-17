c1bf6edfd0cbfe8174fb28a71dbdf10f3f6b0f8d
alexsuen25 <79863030+alexsuen25@users.noreply.github.com>
2023-04-16 20:25:10 -0700

    Delete allergy-scratch directory

--- allergy-scratch/create_table.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE users (id INTEGER, username TEXT NOT NULL, hash TEXT NOT NULL, firstname TEXT, lastname TEXT, account TEXT, PRIMARY KEY(id));
-CREATE TABLE allergy (user_id INTEGER, symbol TEXT, date TEXT);
